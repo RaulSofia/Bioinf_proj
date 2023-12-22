44185bc3153d4911732b5b4a89d59824393844b3
RaulSofia <rauljcsofia@gmail.com>
2023-12-22 01:56:44 +0000

    todos

--- basic_autoencoder.py
+++ basic_autoencoder.py
@@ -1,9 +1,22 @@
+#TODO: work with smiles
+#TODO: smiles to tokens
+#TODO: tokens to embeddings
+#TODO: adjust input to accomodate embeddings
+#TODO: mirror the encoder and decoder
+#TODO: change loss function to binary_crossentropy
+#TODO: find large dataset to preptrain encoder and decoder
+#TODO: create model to predict binding affinity (1, 2, 3 layers)
+#TODO: train this last model on a dataset composed of encoder(tiago's smiles) -> binding affinity
+
+
+
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.layers import Input, Dense, RepeatVector, LSTM, TimeDistributed, Flatten
+from tensorflow.keras.layers import Input, Dense, RepeatVector, LSTM, TimeDistributed, Flatten, Embedding
 from tensorflow.keras.models import Model
 
+EMBEDDING_DIM = 128
 
 def main():
     # Generate example data
@@ -12,10 +25,11 @@ def main():
 
     # Define the model
     input_seq = Input(shape=(10, 1))
+    # encoded = Embedding(10, EMBEDDING_DIM, input_length=10)(input_seq)
     encoded = LSTM(128, return_sequences=True, name="lstm_encoder")(input_seq)
-    encoded = TimeDistributed(Dense(1))(encoded)
+    encoded = TimeDistributed(Dense(EMBEDDING_DIM))(encoded)
     encoded = Flatten()(encoded)
-    encoded = Dense(128, name="encoder_output")(encoded)
+    encoded = Dense(EMBEDDING_DIM, name="encoder_output")(encoded)
 
     decoded = RepeatVector(10)(encoded)
 
@@ -25,7 +39,7 @@ def main():
     # Autoencoder model
     autoencoder = Model(input_seq, decoded)
     autoencoder.summary()
-    autoencoder.compile(optimizer='adam', loss='mean_squared_error')
+    autoencoder.compile(optimizer='adam', loss='mean_squared_error') #binary_crossentropy
     
 
     # Train the model
